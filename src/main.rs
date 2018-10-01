2d8da0601bdc0f8f3b40bb33919ca971d378b9fa
Qian Linfeng <thewawar@gmail.com>
2018-10-01 13:55:21 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,38 @@ extern crate crossbeam_channel as channel;
 
 use std::thread;
 use std::time::Duration;
+use worker::Worker;
 
 fn main() {
     let dummy_controller = DummyWorker::default().start();
     let response = dummy_controller.send_request(("this is a request/response task".to_string(), true));
     println!("Got response: {:?}", response.recv());
-    dummy_controller.send_event(("this is event to send".to_string(), 333));
+    dummy_controller.send(("this is event to send".to_string(), 333));
 
     if let Some(join_handle) = dummy_controller.join_handle {
         join_handle.join().expect("Join dummy worker failed");
     }
     println!(">>> DONE!");
+}
+
+mod worker {
+    use super::channel;
+
+    pub struct RequestTask<A, R> {
+        pub responsor: channel::Sender<R>,
+        pub arguments: A,
+    }
+
+    #[derive(Debug)]
+    pub struct EventTask<A> {
+        pub arguments: A,
+    }
+
+    pub trait Worker {
+        type Controller;
+        // Worker's main loop
+        fn start(self) -> Self::Controller;
+    }
 }
 
 
@@ -22,50 +43,17 @@ type Task1Arguments = (String, bool);
 
 type Task2Arguments = (String, u64);
 
-struct RequestTask<A, R> {
-    responsor: channel::Sender<R>,
-    arguments: A,
-}
-
-#[derive(Debug)]
-struct EventTask<A> {
-    arguments: A,
-}
 
 #[derive(Default)]
 struct DummyWorker {}
 
-
 struct DummyWorkerController {
     join_handle: Option<thread::JoinHandle<()>>,
-    task1_sender: channel::Sender<RequestTask<Task1Arguments, Task1ResponseValue>>,
-    task2_sender: channel::Sender<EventTask<Task2Arguments>>,
+    task1_sender: channel::Sender<worker::RequestTask<Task1Arguments, Task1ResponseValue>>,
+    task2_sender: channel::Sender<worker::EventTask<Task2Arguments>>,
 }
 
-
-pub trait Worker {
-    type Controller;
-    // Worker's main loop
-    fn start(self) -> Self::Controller;
-}
-
-
-pub trait WorkerController {
-    type EventArguments;
-    type RequestArguments;
-    type ResponseValue;
-
-    fn send_request(&self, arguments: Self::RequestArguments) -> channel::Receiver<Self::ResponseValue> {
-        unimplemented!();
-    }
-
-    fn send_event(&self, arguments: Self::EventArguments) {
-        unimplemented!();
-    }
-}
-
-
-impl Worker for DummyWorker {
+impl worker::Worker for DummyWorker {
     type Controller = DummyWorkerController;
 
     fn start(self) -> Self::Controller {
@@ -76,7 +64,7 @@ impl Worker for DummyWorker {
         let join_handle = thread_builder.spawn(move || loop {
             select! {
                 recv(task1_receiver, msg) => match msg {
-                    Some(RequestTask { responsor, arguments }) => {
+                    Some(worker::RequestTask { responsor, arguments }) => {
                         println!("received {:?} from task1", arguments);
                         let return_value = 42;
                         responsor.send(return_value);
@@ -84,7 +72,7 @@ impl Worker for DummyWorker {
                     None => println!("Task1 channel is closed"),
                 }
                 recv(task2_receiver, msg) => match msg {
-                    Some(EventTask { arguments }) => {
+                    Some(worker::EventTask { arguments }) => {
                         println!("received {:?} from task2. Quit after 2 seconds", arguments);
                         thread::sleep(Duration::from_secs(2));
                         break;
@@ -102,21 +90,17 @@ impl Worker for DummyWorker {
     }
 }
 
-impl WorkerController for DummyWorkerController {
-    type EventArguments = Task2Arguments;
-    type RequestArguments = Task1Arguments;
-    type ResponseValue = Task1ResponseValue;
-
-    fn send_request(&self, arguments: Self::RequestArguments) -> channel::Receiver<Self::ResponseValue> {
+impl DummyWorkerController {
+    fn send_request(&self, arguments: Task1Arguments) -> channel::Receiver<Task1ResponseValue> {
         let (sender, receiver) = channel::bounded(1);
-        self.task1_sender.send(RequestTask {
+        self.task1_sender.send(worker::RequestTask {
             responsor: sender,
             arguments,
         });
         receiver
     }
 
-    fn send_event(&self, arguments: Self::EventArguments) {
-        self.task2_sender.send(EventTask { arguments });
+    fn send(&self, arguments: Task2Arguments) {
+        self.task2_sender.send(worker::EventTask { arguments });
     }
 }
