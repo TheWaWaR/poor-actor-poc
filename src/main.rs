b743538ccd0ee747f94dbbc8f126f4b60be6adbd
Qian Linfeng <thewawar@gmail.com>
2018-10-08 11:23:07 +0800

    basic poc

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,28 @@ use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 use std::collections::HashMap;
-use worker::{Worker, RequestTask, EventTask};
+use worker::{Worker, RequestTask};
 
 fn main() {
-    let (simple_handle, simple_controller) = SimpleWorker::default().start();
+    let (pubsub_handle, pubshb_controller) = PubsubWorker::default().start("pubsub worker");
+
+    let (simple_handle, simple_controller) = SimpleWorker::new(pubshb_controller.clone(), "simple")
+        .start("simple");
+    let (simple2_handle, simple2_controller) = SimpleWorker::new(pubshb_controller.clone(), "simple2")
+        .start("simple2");
+
     let response = simple_controller.send_request(("this is a request/response task".to_string(), true));
     println!("Got response: {:?}", response.recv());
-    simple_controller.send(("this is event to send".to_string(), 333));
 
-    simple_handle.join().expect("Join dummy worker failed");
+    thread::sleep_ms(100);
+
+    simple_controller.do_task2(("this is event to send".to_string(), 111));
+    simple2_controller.do_task2(("this is event to send".to_string(), 222));
+
+    simple_handle.join().expect("join simple worker failed");
+    simple2_handle.join().expect("join simple2 worker failed");
+    pubshb_controller.stop();
+    pubsub_handle.join().expect("join pubsub worker failed");
     println!(">>> DONE!");
 }
 
@@ -27,60 +40,64 @@ mod worker {
         pub arguments: A,
     }
 
-    #[derive(Debug)]
-    pub struct EventTask<A> {
-        pub arguments: A,
-    }
-
     pub trait Worker {
         type Controller;
         // Worker's main loop
-        fn start(self) -> (thread::JoinHandle<()>, Self::Controller);
+        fn start<S: ToString>(self, thread_name: S) -> (thread::JoinHandle<()>, Self::Controller);
     }
 }
 
 
+
+/* ================ */
+/* ==== Simple ==== */
+/* ================ */
 type Task1ResponseValue = u32;
 type Task1Arguments = (String, bool);
 
 type Task2Arguments = (String, u64);
 
 
-#[derive(Default)]
 struct SimpleWorker {
+    new_tx_receiver: channel::Receiver<()>,
+    pubsub: PubsubWorkerController,
 }
 
 #[derive(Clone)]
 struct SimpleWorkerController {
     task1_sender: channel::Sender<RequestTask<Task1Arguments, Task1ResponseValue>>,
-    task2_sender: channel::Sender<EventTask<Task2Arguments>>,
+    task2_sender: channel::Sender<Task2Arguments>,
 }
 
 impl Worker for SimpleWorker {
     type Controller = SimpleWorkerController;
 
-    fn start(self) -> (thread::JoinHandle<()>, Self::Controller) {
+    fn start<S: ToString>(self, thread_name: S) -> (thread::JoinHandle<()>, Self::Controller) {
         let (task1_sender, task1_receiver) = channel::bounded(1024);
         let (task2_sender, task2_receiver) = channel::bounded(1024);
 
-        let thread_builder = thread::Builder::new().name("dummy worker".to_string());
+        let thread_builder = thread::Builder::new().name(thread_name.to_string());
         let join_handle = thread_builder.spawn(move || loop {
             select! {
                 recv(task1_receiver, msg) => match msg {
-                    Some(RequestTask { responsor, arguments }) => {
-                        println!("received {:?} from task1", arguments);
-                        let return_value = 42;
-                        responsor.send(return_value);
+                    Some(request) => {
+                        self.handle_request(request);
                     },
                     None => println!("Task1 channel is closed"),
                 }
                 recv(task2_receiver, msg) => match msg {
-                    Some(EventTask { arguments }) => {
-                        println!("received {:?} from task2. Quit after 2 seconds", arguments);
-                        thread::sleep(Duration::from_secs(2));
-                        break;
+                    Some(arguments) => {
+                        if self.handle_task2(arguments) {
+                            break;
+                        }
                     },
                     None => println!("Task2 channel is closed"),
+                }
+                recv(self.new_tx_receiver, msg) => match msg {
+                    Some(_) => {
+                        self.handle_new_tx();
+                    },
+                    None => println!("new tx notify channel is closed"),
                 }
             }
         }).expect("Start dummy worker failed");
@@ -95,6 +112,31 @@ impl Worker for SimpleWorker {
     }
 }
 
+impl SimpleWorker {
+    pub fn new(pubsub: PubsubWorkerController, new_tx_name: &str) -> SimpleWorker {
+        let new_tx_receiver = pubsub.subscribe_net_tx(new_tx_name.to_string());
+        SimpleWorker { new_tx_receiver, pubsub }
+    }
+
+    pub fn handle_request(&self, request: RequestTask<Task1Arguments, Task1ResponseValue>) {
+        let RequestTask { responsor, arguments } = request;
+        println!("received {:?} from task1", arguments);
+        let return_value = 42;
+        responsor.send(return_value);
+        self.pubsub.notify_new_tx();
+    }
+
+    pub fn handle_task2(&self, arguments: Task2Arguments) -> bool {
+        println!("received {:?} from task2. Quit after 2 seconds", arguments);
+        thread::sleep(Duration::from_secs(2));
+        true
+    }
+
+    pub fn handle_new_tx(&self) {
+        println!("new tx received in SimpleWorker");
+    }
+}
+
 impl SimpleWorkerController {
     pub fn send_request(&self, arguments: Task1Arguments) -> channel::Receiver<Task1ResponseValue> {
         let (sender, receiver) = channel::bounded(1);
@@ -105,8 +147,8 @@ impl SimpleWorkerController {
         receiver
     }
 
-    pub fn send(&self, arguments: Task2Arguments) {
-        self.task2_sender.send(EventTask { arguments });
+    pub fn do_task2(&self, arguments: Task2Arguments) {
+        self.task2_sender.send(arguments);
     }
 }
 
@@ -115,14 +157,15 @@ impl SimpleWorkerController {
 /* ==== PubSub ==== */
 /* ================ */
 
-struct PubsubWorker {
-}
+#[derive(Default)]
+struct PubsubWorker {}
 
 #[derive(Clone)]
 struct PubsubWorkerController {
-    new_tx_register: channel::Sender<EventTask<(String, channel::Sender<()>)>>,
-    new_tip_register: channel::Sender<EventTask<(String, channel::Sender<Arc<Vec<u8>>>)>>,
-    switch_fork_register: channel::Sender<EventTask<(String, channel::Sender<Arc<Vec<u32>>>)>>,
+    signal: channel::Sender<()>,
+    new_tx_register: channel::Sender<RequestTask<(String, channel::Sender<()>), ()>>,
+    new_tip_register: channel::Sender<RequestTask<(String, channel::Sender<Arc<Vec<u8>>>), ()>>,
+    switch_fork_register: channel::Sender<RequestTask<(String, channel::Sender<Arc<Vec<u32>>>), ()>>,
     new_tx_notifier: channel::Sender<()>,
     new_tip_notifier: channel::Sender<Arc<Vec<u8>>>,
     switch_fork_notifier: channel::Sender<Arc<Vec<u32>>>,
@@ -131,50 +174,78 @@ struct PubsubWorkerController {
 impl Worker for PubsubWorker {
     type Controller = PubsubWorkerController;
 
-    fn start(self) -> (thread::JoinHandle<()>, Self::Controller) {
+    fn start<S: ToString>(self, thread_name: S) -> (thread::JoinHandle<()>, Self::Controller) {
+        let (signal_sender, signal_receiver) = channel::bounded::<()>(1);
         let (register1_sender, register1_receiver) = channel::bounded(2);
         let (register2_sender, register2_receiver) = channel::bounded(2);
         let (register3_sender, register3_receiver) = channel::bounded(2);
         let (event1_sender, event1_receiver) = channel::bounded::<()>(128);
-        let (event2_sender, event2_receiver) = channel::bounded(128);
-        let (event3_sender, event3_receiver) = channel::bounded(128);
+        let (event2_sender, event2_receiver) = channel::bounded::<Arc<Vec<u8>>>(128);
+        let (event3_sender, event3_receiver) = channel::bounded::<Arc<Vec<u32>>>(128);
 
-        let thread_builder = thread::Builder::new().name("pubsub worker".to_string());
+        let thread_builder = thread::Builder::new().name(thread_name.to_string());
         let join_handle = thread_builder.spawn(move || {
             let mut new_tx_subscribers: HashMap<String, channel::Sender<()>> = HashMap::new();
             let mut new_tip_subscribers: HashMap<String, channel::Sender<Arc<Vec<u8>>>> = HashMap::new();
             let mut switch_fork_subscribers: HashMap<String, channel::Sender<Arc<Vec<u32>>>> = HashMap::new();
             loop {
                 select! {
+                    recv(signal_receiver, msg) => {
+                        break;
+                    }
+
                     recv(register1_receiver, msg) => match msg {
-                        Some(EventTask{ arguments: (name, sender) }) => {
+                        Some(RequestTask { responsor, arguments: (name, sender) }) => {
                             println!("Register event1 {:?}", name);
                             new_tx_subscribers.insert(name, sender);
+                            responsor.send(());
                         },
                         None => println!("Register 1 channel is closed"),
                     }
                     recv(register2_receiver, msg) => match msg {
-                        Some(EventTask{ arguments: (name, sender) }) => {
+                        Some(RequestTask { responsor, arguments: (name, sender)}) => {
                             println!("Register event2 {:?}", name);
                             new_tip_subscribers.insert(name, sender);
+                            responsor.send(());
                         },
                         None => println!("Register 2 channel is closed"),
                     }
                     recv(register3_receiver, msg) => match msg {
-                        Some(EventTask{ arguments: (name, sender) }) => {
+                        Some(RequestTask { responsor, arguments: (name, sender)}) => {
                             println!("Register event3 {:?}", name);
                             switch_fork_subscribers.insert(name, sender);
+                            responsor.send(());
                         },
                         None => println!("Register 3 channel is closed"),
                     }
+
                     recv(event1_receiver, msg) => match msg {
                         Some(msg) => {
                             println!("event new tx {:?}", msg);
-                            for subscriber in new_tx_subscribers.values() {
+                            for (name, subscriber) in &new_tx_subscribers {
+                                println!("Send new tx to: {}", name);
                                 subscriber.send(msg.clone());
                             }
                         },
                         None => println!("event 1 channel is closed"),
+                    }
+                    recv(event2_receiver, msg) => match msg {
+                        Some(msg) => {
+                            println!("event new tip {:?}", msg);
+                            for subscriber in new_tip_subscribers.values() {
+                                subscriber.send(msg.clone());
+                            }
+                        },
+                        None => println!("event 1 channel is closed"),
+                    }
+                    recv(event3_receiver, msg) => match msg {
+                        Some(msg) => {
+                            println!("event switch fork {:?}", msg);
+                            for subscriber in switch_fork_subscribers.values() {
+                                subscriber.send(msg.clone());
+                            }
+                        },
+                        None => println!("event 3 channel is closed"),
                     }
                 }
             }
@@ -189,28 +260,42 @@ impl Worker for PubsubWorker {
                 new_tx_notifier: event1_sender,
                 new_tip_notifier: event2_sender,
                 switch_fork_notifier: event3_sender,
+                signal: signal_sender,
             }
         )
     }
 }
 
 impl PubsubWorkerController {
+    pub fn stop(&self) {
+        self.signal.send(());
+    }
+
     pub fn subscribe_net_tx<S: ToString>(&self, name: S) -> channel::Receiver<()> {
         let name = name.to_string();
+        let (responsor, response) = channel::bounded(1);
         let (event1_sender, event1_receiver) = channel::bounded::<()>(128);
-        self.new_tx_register.send(EventTask { arguments: (name, event1_sender)});
+        self.new_tx_register.send(RequestTask { responsor, arguments: (name, event1_sender)});
+        // Ensure the subscriber is registered.
+        let _ = response.recv();
         event1_receiver
     }
     pub fn subscribe_net_tip<S: ToString>(&self, name: S) -> channel::Receiver<Arc<Vec<u8>>> {
         let name = name.to_string();
+        let (responsor, response) = channel::bounded(1);
         let (event2_sender, event2_receiver) = channel::bounded::<Arc<Vec<u8>>>(128);
-        self.new_tip_register.send(EventTask { arguments: (name, event2_sender)});
+        self.new_tip_register.send(RequestTask { responsor, arguments: (name, event2_sender)});
+        // Ensure the subscriber is registered.
+        let _ = response.recv();
         event2_receiver
     }
     pub fn subscribe_switch_fork<S: ToString>(&self, name: S) -> channel::Receiver<Arc<Vec<u32>>> {
         let name = name.to_string();
+        let (responsor, response) = channel::bounded(1);
         let (event3_sender, event3_receiver) = channel::bounded::<Arc<Vec<u32>>>(128);
-        self.switch_fork_register.send(EventTask { arguments: (name, event3_sender)});
+        self.switch_fork_register.send(RequestTask { responsor, arguments: (name, event3_sender)});
+        // Ensure the subscriber is registered.
+        let _ = response.recv();
         event3_receiver
     }
 
